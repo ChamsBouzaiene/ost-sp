fad359d6c724b6d448715823a5864400025208a0
chams <alchemist107@gmail.com>
2018-12-17 09:40:59 +0100

    first commit

---
+++ ost-sp/src/features/front/home/components/Home.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+
+
+class Home extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        
+        <p className="App-intro">
+         OST-SP
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
