babe80f3a35bd62fe4d70251c966d6f3b86588fe
Bouzaine Chamsddine <alchemist107@gmail.com>
2018-12-19 10:50:33 +0100

    added favicon , fixed auth component style

--- ost-sp/src/features/front/auth/components/Auth.tsx
+++ ost-sp/src/features/front/auth/components/Auth.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import "./Auth.css"
 
 class Auth extends React.Component {
   public render() {
     return (
-      <div className="app flex-row align-items-center">
+      <div className="app d-flex flex-row align-items-center">
         <Container>
           <Row className="justify-content-center">
             <Col md="8">
--- ost-sp/src/features/main/components/App.tsx
+++ ost-sp/src/features/main/components/App.tsx
@@ -1,7 +1,8 @@
+import "font-awesome/css/font-awesome.min.css"
 import * as React from 'react';
+import "simple-line-icons/css/simple-line-icons.css"
 import './App.css';
 import Routes from "./Routes"
-
 
 
 class App extends React.Component {
