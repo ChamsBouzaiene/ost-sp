651857d2eb44a020adf921b9aae2e32728a2c31e
Bouzaine Chamsddine <alchemist107@gmail.com>
2019-01-11 18:49:00 +0100

    entity is at 50%

--- ost-sp/src/features/back/entity/components/List.tsx
+++ ost-sp/src/features/back/entity/components/List.tsx
@@ -1,17 +1,30 @@
 import * as React from "react";
 import * as Actions from "../actions";
-import { connect, MapDispatchToProps } from "react-redux";
+import { connect, MapDispatchToProps, MapStateToProps } from "react-redux";
 import { ThunkDispatch } from "redux-thunk";
 import { IState } from "src/shared/store";
+import Entity from "src/data/Entity";
 
-type Props = DispatchProps & TOwnProps;
+type Props = DispatchProps & TOwnProps & TStateProps;
 
 class List extends React.Component<Props> {
   componentDidMount() {
     this.props.onDidMount();
   }
   render() {
-    return <div>Entity</div>;
+    return (
+      <div>
+        {this.props.entities &&
+          this.props.entities.map(entity =>
+            Object.entries(entity).map(([k, v]) => (
+              <div>
+                <label htmlFor="">{k}</label>
+                <input value={v} />
+              </div>
+            ))
+          )}
+      </div>
+    );
   }
 }
 
@@ -30,7 +43,17 @@ const mapDispatchToProps: MapDispatchToProps<DispatchProps, TOwnProps> = (
   }
 });
 
+interface TStateProps {
+  entities: Entity[];
+}
+
+const mapStateToProp: MapStateToProps<TStateProps, TOwnProps, IState> = (
+  state: IState
+) => ({
+  entities: state.entity.entities
+});
+
 export default connect(
-  null,
+  mapStateToProp,
   mapDispatchToProps
 )(List);
--- ost-sp/src/features/back/entity/reducer.ts
+++ ost-sp/src/features/back/entity/reducer.ts
@@ -1,22 +1,12 @@
 import { Reducer } from "redux";
 import * as Actions from "./actions";
 
-const initAuth = () => {
-  let savedToken;
-  try {
-    savedToken = JSON.parse(localStorage.getItem("token") as any);
-  } catch (err) {
-    savedToken = null;
-  }
-  return savedToken || {};
-};
-
-const auth: Reducer<any, Actions.All> = (state = initAuth(), action) => {
+const entity: Reducer<any, Actions.All> = (state = [], action) => {
   switch (action.type) {
     case Actions.GET_ALL_ENTITYS_SUCCESS:
-      return { ...state, entity: action.entity };
+      return { ...state, entities: action.entity };
   }
   return state;
 };
 
-export default auth;
+export default entity;
